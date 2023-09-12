40a8d0f2bddceb21d92b61f6d01348331e694ce7
ogochukwu alzoie <ogochukwu.alozie@decagon.dev>
2023-09-12 21:52:47 +0100

    just getting started

--- App.js
+++ App.js
@@ -1,35 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
 import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native';
+import Header from './components/Header';
 
 export default function App() {
-     const [people, setPeople] = useState([
-          {name: "Bright", id: "1"},
-          {name: "Paul", id: "2"},
-          {name: "Ralph", id: "3"},
-          {name: "Uche", id: "4"},
-          {name: "Beh", id: "5"},
-          {name: "Greg", id: "6"},
+     const [todos, setTodos] = useState([
+          {Text: "Onvail Frontend", key: "1"},
+          {Text: "Take my bath", key: "2"},
+          {Text: "Read the book i bought", id: "3"}
      ])
      
-     const pressHandler = (id) => {
-          setPeople((prevPeople) => {
-               return prevPeople.filter(person => person.id != id)
-          })
-     }
-
   return (
     <View style={styles.container}>
-          <FlatList 
-               numColumns={2}
-               keyExtractor={(item) => item.id}
-               data={people}
-               renderItem={({item}) => (
-                    <TouchableOpacity onPress={() => pressHandler(item.id)}>
-                         <Text style={styles.item}>{item.name}</Text>
-                    </TouchableOpacity>
-               )}
-          />
+          <Header />
+          <View style={styles.content}>
+               {/* forms */}
+                <View style={style.list}>
+                  <FlatList 
+                    data={todos}
+                    renderItem={({item}) => (
+                      <Text>{item.text}</Text>
+                    )}
+                  />
+                </View>
+          </View>
      </View>
   );
 }
@@ -38,17 +32,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    paddingTop: 40,
-    paddingHorizontal: 20,
-//     alignItems: 'center',
-//     justifyContent: 'center',
   },
-  item: {
-     marginTop: 24,
-     padding: 30,
-     backgroundColor: "pink",
-     fontSize: 24,
-     marginHorizontal: 10,
-     marginTop: 24
+  content: {
+    padding: 40,
+  },
+  list: {
+    marginTop: 20,
   }
+  
 });
---
+++ components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { StyleSheet, View, Text} from 'react-native-web'
+
+function Header() {
+  return (
+    <View style={styles.index}>
+      <Text style>
+
+      </Text>
+
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default Header
